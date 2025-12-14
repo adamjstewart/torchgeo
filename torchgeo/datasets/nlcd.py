3ee51e9bd17f5eaa6f34ec5d3d1a803e9bf3e23e
Adam J. Stewart <ajstewart426@gmail.com>
2025-12-14 16:55:08 +0100

    Update plot
    Signed-off-by: Adam J. Stewart <ajstewart426@gmail.com>

--- torchgeo/datasets/nlcd.py
+++ torchgeo/datasets/nlcd.py
@@ -239,10 +239,7 @@ class NLCD(RasterDataset):
             )
 
     def plot(
-        self,
-        sample: dict[str, Any],
-        show_titles: bool = True,
-        suptitle: str | None = None,
+        self, sample: Sample, show_titles: bool = True, suptitle: str | None = None
     ) -> Figure:
         """Plot a sample from the dataset.
 
