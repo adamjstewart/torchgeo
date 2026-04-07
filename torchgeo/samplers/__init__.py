0024cb82eeed79017c189faf7000bd7dc49ba49c
Adam J. Stewart <ajstewart426@gmail.com>
2026-04-07 18:33:03 +0200

    Add import aliases
    Signed-off-by: Adam J. Stewart <ajstewart426@gmail.com>

--- torchgeo/samplers/__init__.py
+++ torchgeo/samplers/__init__.py
@@ -3,18 +3,39 @@
 
 """TorchGeo samplers."""
 
+from .base import GeoSampler, SpatialSampler, SpatioTemporalSampler, TemporalSampler
 from .batch import BatchGeoSampler, RandomBatchGeoSampler
 from .constants import Units
-from .single import GeoSampler, GridGeoSampler, PreChippedGeoSampler, RandomGeoSampler
+from .single import GridGeoSampler, PreChippedGeoSampler, RandomGeoSampler
+from .spatial import GridSpatialSampler, RandomSpatialSampler
+from .temporal import (
+    RandomPeriodSampler,
+    RandomTimedeltaSampler,
+    RandomTimestampSampler,
+    SequentialPeriodSampler,
+    SequentialTimedeltaSampler,
+    SequentialTimestampSampler,
+)
 from .utils import get_random_bounding_box, tile_to_chips
 
 __all__ = (
     'BatchGeoSampler',
     'GeoSampler',
     'GridGeoSampler',
+    'GridSpatialSampler',
     'PreChippedGeoSampler',
     'RandomBatchGeoSampler',
     'RandomGeoSampler',
+    'RandomPeriodSampler',
+    'RandomSpatialSampler',
+    'RandomTimedeltaSampler',
+    'RandomTimestampSampler',
+    'SequentialPeriodSampler',
+    'SequentialTimedeltaSampler',
+    'SequentialTimestampSampler',
+    'SpatialSampler',
+    'SpatioTemporalSampler',
+    'TemporalSampler',
     'Units',
     'get_random_bounding_box',
     'tile_to_chips',
